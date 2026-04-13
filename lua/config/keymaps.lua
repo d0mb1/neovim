171092056e12e6adaee2966ab6511a165ea647e9
d0mb1 <sebastianrance+github@pm.me>
2026-04-13 19:54:46 +0200

    .

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -97,3 +97,9 @@ local function pack_clean()
 end
 
 vim.keymap.set("n", "<leader>Pc", pack_clean, { desc = "Clean" })
+
+vim.keymap.set("n", "<leader>R", function()
+  local session = vim.fn.stdpath("state") .. "/restart_session.vim"
+  vim.cmd("mksession! " .. vim.fn.fnameescape(session))
+  vim.cmd("restart source " .. vim.fn.fnameescape(session))
+end, { desc = "Restart Neovim" })
